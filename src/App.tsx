75e2e7c4a3442100f6c07a61f42d8f0ced77e0b9
pedro <pedroantoniofpa22@gmail.com>
2026-09-22 16:43:49 -0300

    feat: implement authentication components and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import router from "./components/Routes";
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
